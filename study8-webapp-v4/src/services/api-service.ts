9d6aec4a546516b2f53ec4c2f6fbfa849e142913
HuyNH <huynh@unit.com.vn>
2025-07-11 16:58:50 +0700

    [HuyNH] Add api-service

---
+++ study8-webapp-v4/src/services/api-service.ts
@@ -0,0 +1,31 @@
+import {axiosClient} from "../libs/axios/axios-client";
+
+export const apiService = {
+    get: <T>(url: string, params?: any) => handleRequest<T>(axiosClient.get(url, { params })),
+
+    post: <T>(url: string, data?: any) => handleRequest<T>(axiosClient.post(url, data)),
+
+    put: <T>(url: string, data?: any) => handleRequest<T>(axiosClient.put(url, data)),
+
+    delete: <T>(url: string) => handleRequest<T>(axiosClient.delete(url)),
+
+    postFormData: <T>(url: string, formData: FormData) =>
+        handleRequest<T>(
+            axiosClient.post(url, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            })
+        ),
+};
+
+
+
+const handleRequest = async <T>(request: Promise<any>): Promise<T> => {
+    try {
+        const response = await request;
+        return response.data as T;
+    } catch (error: any) {
+        throw error.response?.data?.message || 'Unexpected error';
+    }
+};
