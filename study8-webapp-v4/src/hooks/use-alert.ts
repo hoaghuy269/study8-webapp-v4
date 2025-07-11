17897173389985e97df283cf325b1f912567da8b
huynh <hoaghuy269@gmail.com>
2025-07-11 23:36:06 +0700

    [HuyNH] Add ALERT_SEVERITY

--- study8-webapp-v4/src/hooks/use-alert.ts
+++ study8-webapp-v4/src/hooks/use-alert.ts
@@ -2,4 +2,17 @@ import { useContext } from 'react';
 
 import { SnackbarContext } from '../components/snackbar/snackbar-provider';
 
-export const useAlert = () => useContext(SnackbarContext);
+export const useAlert = () => {
+  const context = useContext(SnackbarContext);
+  if (!context) {
+    throw new Error('use-alert.ts | Error | useAlert must be used within a SnackbarProvider');
+  }
+  return context;
+};
+
+export const ALERT_SEVERITY = {
+  SUCCESS: 'success',
+  ERROR: 'error',
+  WARNING: 'warning',
+  INFO: 'info',
+} as const;
