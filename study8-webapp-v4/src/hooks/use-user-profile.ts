1a43db46b9f403b4154436240f2c03dcea14339a
HuyNH <huynh@unit.com.vn>
2025-07-14 15:31:21 +0700

    [HuyNH] Update hook useUserProfile

--- study8-webapp-v4/src/hooks/use-user-profile.ts
+++ study8-webapp-v4/src/hooks/use-user-profile.ts
@@ -2,4 +2,10 @@ import {useContext} from "react";
 
 import {UserProfileContext} from "../components/user-profile/user-profile-provider";
 
-export const useUserProfile = () => useContext(UserProfileContext);
+export const useUserProfile = () => {
+    const context = useContext(UserProfileContext);
+    if (!context) {
+        throw new Error('use-user-profile.ts | Error | useUserProfile must be used within a UserProfileProvider');
+    }
+    return context;
+};
