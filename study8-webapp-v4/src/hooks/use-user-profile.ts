3a49cbf9b6c9b535f9e1d7a146f810a9c420e748
HuyNH <huynh@unit.com.vn>
2025-07-14 15:31:39 +0700

    [HuyNH] Format code

--- study8-webapp-v4/src/hooks/use-user-profile.ts
+++ study8-webapp-v4/src/hooks/use-user-profile.ts
@@ -1,11 +1,13 @@
-import {useContext} from "react";
+import { useContext } from 'react';
 
-import {UserProfileContext} from "../components/user-profile/user-profile-provider";
+import { UserProfileContext } from '../components/user-profile/user-profile-provider';
 
 export const useUserProfile = () => {
-    const context = useContext(UserProfileContext);
-    if (!context) {
-        throw new Error('use-user-profile.ts | Error | useUserProfile must be used within a UserProfileProvider');
-    }
-    return context;
+  const context = useContext(UserProfileContext);
+  if (!context) {
+    throw new Error(
+      'use-user-profile.ts | Error | useUserProfile must be used within a UserProfileProvider'
+    );
+  }
+  return context;
 };
