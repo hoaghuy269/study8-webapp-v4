51b2d9676bb3a1c25b7dd2cc4e6b1b309040dab0
huynh <hoaghuy269@gmail.com>
2025-07-15 13:14:05 +0700

    [HuyNH] Update app name

--- study8-webapp-v4/src/config-global.ts
+++ study8-webapp-v4/src/config-global.ts
@@ -10,6 +10,6 @@ export type ConfigValue = {
 // ----------------------------------------------------------------------
 
 export const CONFIG: ConfigValue = {
-  appName: 'Minimal UI',
+  appName: 'Study8',
   appVersion: packageJson.version,
 };
