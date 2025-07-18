8034f83e574b2cda5bd16168e8aa027f89e3774f
huynh <hoaghuy269@gmail.com>
2025-07-18 23:35:16 +0700

    [HuyNH] Add DEFAULT_CIRCULAR_PROGRESS_SIZE

--- study8-webapp-v4/src/constant/pagination.ts
+++ study8-webapp-v4/src/constant/pagination.ts
@@ -4,3 +4,4 @@ export const DEFAULT_PAGE_SIZE = 30;
 export const DEFAULT_ORDER_BY = 'DEFAULT';
 export const DEFAULT_SEARCH = '';
 export const DEFAULT_WORKSPACE = 'TEACHER';
+export const DEFAULT_CIRCULAR_PROGRESS_SIZE = 20;
