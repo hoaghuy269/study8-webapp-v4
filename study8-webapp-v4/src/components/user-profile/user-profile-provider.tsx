83716fb04b4911f01a1366c113d78ddf49d3763f
huynh <hoaghuy269@gmail.com>
2025-07-21 21:06:48 +0700

    [HuyNH] Refactor user-profile-provider.tsx

--- study8-webapp-v4/src/components/user-profile/user-profile-provider.tsx
+++ study8-webapp-v4/src/components/user-profile/user-profile-provider.tsx
@@ -2,10 +2,8 @@ import type { ReactNode } from 'react';
 
 import { useMemo, useState, useEffect, createContext } from 'react';
 
-import { useRouter } from '../../routes/hooks';
 import { apiService } from '../../services/api-service';
 import { API_USER_PROFILE } from '../../constant/api-path';
-import { useAlert, ALERT_SEVERITY } from '../../hooks/use-alert';
 
 import type { ApiResponse } from '../../libs/types/api-response';
 import type { UserProfile } from '../../libs/types/user-profile';
@@ -13,8 +11,6 @@ import type { UserProfile } from '../../libs/types/user-profile';
 export const UserProfileContext = createContext<{ userProfile: UserProfile | null } | null>(null);
 
 export const UserProfileProvider = ({ children }: { children: ReactNode }) => {
-  const router = useRouter();
-  const { showSnackbar } = useAlert();
   const [userProfile, setUserProfile] = useState<UserProfile | null>(null);
 
   useEffect(() => {
@@ -23,10 +19,8 @@ export const UserProfileProvider = ({ children }: { children: ReactNode }) => {
       .then((res) => setUserProfile(res.data))
       .catch((err) => {
         console.error('user-profile-provider.tsx | Error', err);
-        router.push('/sign-in');
-        showSnackbar('Session expired', ALERT_SEVERITY.ERROR);
       });
-  }, [router, showSnackbar]);
+  }, []);
 
   const contextValue = useMemo(() => ({ userProfile }), [userProfile]);
 
