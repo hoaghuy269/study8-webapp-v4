0a5a034328a02c4311242316e47ff12ed4400aea
HuyNH <huynh@unit.com.vn>
2025-07-23 15:16:15 +0700

    [HuyNH] Refactor header class-detail-view.tsx

--- study8-webapp-v4/src/sections/class-detail/view/class-detail-view.tsx
+++ study8-webapp-v4/src/sections/class-detail/view/class-detail-view.tsx
@@ -5,10 +5,10 @@ import { useParams } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import { Tab, Tabs } from '@mui/material';
-import Skeleton from '@mui/material/Skeleton';
 import { TabPanel, TabContext } from '@mui/lab';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 import { useRouter } from '../../../routes/hooks';
 import { Iconify } from '../../../components/iconify';
@@ -22,7 +22,6 @@ import type { ClassResponse } from '../../class/type/class-response';
 export function ClassDetailView() {
   const router = useRouter();
   const { classId } = useParams();
-  const [classLoading, setClassLoading] = useState(true);
   const [classDetail, setClassDetail] = useState<ClassResponse | null>(null);
   const [tab, setTab] = useState('topic');
   const { getClassDetail } = useClassDetailService();
@@ -30,7 +29,6 @@ export function ClassDetailView() {
   useEffect(() => {
     const fetchClassDetail = async () => {
       try {
-        setClassLoading(true);
         const response = await getClassDetail(Number(classId));
 
         if (response) {
@@ -38,23 +36,12 @@ export function ClassDetailView() {
         }
       } catch (error) {
         router.push(`/404`);
-      } finally {
-        setClassLoading(false);
       }
     };
 
     fetchClassDetail();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [classId, router]);
-
-  const renderSkeleton = () => (
-    <Box display="flex" alignItems="center" mb={2}>
-      <Box flexGrow={1}>
-        <Skeleton variant="text" width={300} height={40} />
-      </Box>
-      <Skeleton variant="circular" width={28} height={28} />
-    </Box>
-  );
 
   const renderTabs = () => (
     <Box sx={{ maxWidth: '100%' }}>
@@ -78,38 +65,21 @@ export function ClassDetailView() {
     </Box>
   );
 
-  const renderHeader = () => {
-    if (classLoading) {
-      renderSkeleton();
-    }
-    return (
-      <Box display="flex" alignItems="center" mb={2}>
-        <Typography variant="h5" flexGrow={1}>
-          <Link
-            component="button"
-            variant="h5"
-            onClick={() => router.push('/class')}
-            sx={{
-              cursor: 'pointer',
-              color: 'primary.main',
-              textDecoration: 'underline',
-              '&:hover': { color: 'primary.dark' },
-            }}
-          >
-            {t('text.class')}
-          </Link>
-          <Typography variant="h5" component="span" color="text.secondary">
-            {' > '}
-            {classDetail?.name}
-          </Typography>
-        </Typography>
+  const renderHeader = () => (
+    <Box display="flex" alignItems="center" mb={2}>
+      <Link component="button" variant="h5" underline="hover" onClick={() => router.push('/class')}>
+        {t('text.class')}
+      </Link>
+      <ChevronRightIcon sx={{ mx: 1, verticalAlign: 'middle' }} />
+      <Typography variant="h5" flexGrow={1}>
+        {classDetail?.name}
+      </Typography>
 
-        <IconButton onClick={() => alert('Mở cài đặt')}>
-          <Iconify icon="mdi:cog" width="28px" color="#000" />
-        </IconButton>
-      </Box>
-    );
-  };
+      <IconButton onClick={() => alert('Mở cài đặt')}>
+        <Iconify icon="mdi:cog" width="28px" color="#000" />
+      </IconButton>
+    </Box>
+  );
 
   return (
     <DashboardContent>
