823e1179a19e81c5c091463d355295b6d99b0124
huynh <hoaghuy269@gmail.com>
2025-07-24 21:35:23 +0700

    [HuyNH] Add DEFAULT_CONTENT_LINE

--- study8-webapp-v4/src/constant/pagination.ts
+++ study8-webapp-v4/src/constant/pagination.ts
@@ -5,3 +5,4 @@ export const DEFAULT_ORDER_BY = 'DEFAULT';
 export const DEFAULT_SEARCH = '';
 export const DEFAULT_WORKSPACE = 'TEACHER';
 export const DEFAULT_CIRCULAR_PROGRESS_SIZE = 20;
+export const DEFAULT_CONTENT_LINE = 3;
